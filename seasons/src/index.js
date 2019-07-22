3391e517b8d9f11e255b0fd22059fe2e75a78fa0
PMarchante <35638610+PMarchante@users.noreply.github.com>
2019-07-22 15:36:06 -0400

    conditional state rendoring

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -5,18 +5,32 @@ class APP extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { lat: null };
+    this.state = { lat: null, errorMsg: '' };
     window.navigator.geolocation.getCurrentPosition(
       //to update the state we MUST call the set state method
-      (position) => {
-        this.setState({ lat: position.coords.latitude });
+      //place is just the name we gave an object
+      //place is a call back function, which means it will update when it finishes
+      //getting what it does
+      (place) => {
+        this.setState({ lat: place.coords.latitude });
       },
-      (err) => console.log(err)
+      (err) => {
+        this.setState({ errorMsg: err.message });
+      }
     );
   }
 
+  //will conditionally render the state depending on the info we have
   render() {
-    return <div>Latitude: {this.state.lat}</div>;
+    if (this.state.errorMsg && !this.state.lat) {
+      return <div>Error: {this.state.errorMsg}</div>;
+    }
+
+    if (!this.state.errorMsg && this.state.lat) {
+      return <div>latitude: {this.state.lat}</div>;
+    }
+
+    return <div>Loading</div>;
   }
 }
 
