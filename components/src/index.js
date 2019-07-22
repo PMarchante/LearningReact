bc9e2298279f18efa5561318837da03259d40d77
PMarchante <35638610+PMarchante@users.noreply.github.com>
2019-07-22 08:55:04 -0400

    first Commit

---
+++ components/src/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import CommentDetail from "./CommentDetail";
+import faker from "faker";
+
+const App = () => {
+  return (
+    <div className='ui container comments'>
+      {/**this is how we call another jsx file */}
+      <CommentDetail
+        author='jake'
+        time='10:40'
+        comment='WOAH!'
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author='bill'
+        time='12:50'
+        comment='This is great!'
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author='sue'
+        time='4:32'
+        comment='YAHOO!'
+        avatar={faker.image.avatar()}
+      />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
