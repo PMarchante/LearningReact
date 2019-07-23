bf64898cef270d86f0dc1ec6f6d6012e593c52c8
Pedro Marchante <pmarc560@gmail.com>
2019-07-22 21:28:55 -0400

    added seasons

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,25 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
 class APP extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = { lat: null, errorMsg: '' };
-
-    //to update the state we MUST call the set state method
-    //place is just the name we gave an object
-    //place is a call back function, which means it will update when it finishes
-    //getting what it does
-  }
+  //state initialization
+  state = { lat: null, errorMsg: "" };
 
   //will execute one time put data loading stuff here
   componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (place) => {
-        this.setState({ lat: place.coords.latitude });
-      },
-      (err) => this.setState({ errorMsg: err.message })
+      place => this.setState({ lat: place.coords.latitude }),
+      err => this.setState({ errorMsg: err.message })
     );
   }
 
@@ -30,11 +20,11 @@ class APP extends React.Component {
     }
 
     if (!this.state.errorMsg && this.state.lat) {
-      return <div>latitude: {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat} />;
     }
 
     return <div>Loading</div>;
   }
 }
 
-ReactDOM.render(<APP />, document.querySelector('#root'));
+ReactDOM.render(<APP />, document.querySelector("#root"));
