0ec49320a076387ffd70ebadbf1df80db9aa6dab
PMarchante <35638610+PMarchante@users.noreply.github.com>
2019-07-23 09:29:56 -0400

    season config function to handle ternary expressio

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,19 +1,32 @@
-import React from "react";
+import React from 'react';
 
-const getSeason = (lat, month) => {
-  if (month > 2 && month < 9) {
-    return lat > 0 ? "Summer" : "Winter";
-  } else {
-    return lat > 0 ? "Winter" : "Summer";
+const seasonConfig = {
+  Summer: {
+    text: 'Lets hit the beach',
+    iconName: 'sun'
+  },
+  Winter: {
+    text: 'burr, it is chilly',
+    iconName: 'snowflake'
   }
 };
 
-const SeasonDisplay = props => {
-  const season = getSeason(props.lat, new Date().getMonth());
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? 'Summer' : 'Winter';
+  } else {
+    return lat > 0 ? 'Winter' : 'Summer';
+  }
+};
 
+const SeasonDisplay = (props) => {
+  const season = getSeason(props.lat, new Date().getMonth());
+  const { text, iconName } = seasonConfig[season];
   return (
     <div>
-      {season === "Winter" ? "Burr, it is chilly" : "Lets hit the beach"}
+      <i className={`${iconName} icon`} />
+      <h1>{text}</h1>
+      <i className={`${iconName} icon`} />
     </div>
   );
 };
